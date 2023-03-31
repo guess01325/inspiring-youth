b7e256239073a42da738a513c06fca485ec34a7f
guess01325 <guess01325@gmail.com>
2023-03-31 04:45:21 -0400

    working and almost finished with mentoring container

---
+++ client/src/containers/MentoringsContainer.jsx
@@ -0,0 +1,38 @@
+import {Route, Routes, useParams, useNavigate, Outlet, Link } from "react-router-dom"; 
+import { useState, useEffect
+ } from "react";
+
+import { getAllMentorings, postMentorings, deleteMentorings } from "../services/Mentoring";
+
+export default function MentoringsContainer (props){
+   const [mentorInfo, setMentorInfo] = useState([]);
+   const params = useParams
+   const {id} = params; 
+   const history = useNavigate()
+
+   useEffect(() => {
+    const fetchMentoring = async () => {
+        const mentoringList = await getAllMentorings();
+        setMentorInfo(mentoringList)
+    }
+    fetchMentoring();
+   }, [])
+
+   const handleCreateMentorings = async (formData) => {
+    const mentoringItems = await postMentorings(formData);
+    setMentorInfo((prevState) => [...prevState, mentoringItems]); 
+   }
+
+   const handleDeleteMentorings = async (id) => {
+    await deleteMentorings(id);
+    setMentorInfo((prevState) => prevState.filter((mentoring) => mentoring.id != id ))
+   }
+   
+   
+   return (
+    <div>
+        <Outlet
+        context={[mentorInfo,handleCreateMentorings, handleDeleteMentorings]}/>
+    </div>
+   )
+}
