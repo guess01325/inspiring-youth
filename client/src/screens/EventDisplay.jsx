f3168ce187a89a3641800a825dc1706c0e995f52
guess01325 <guess01325@gmail.com>
2023-08-12 15:03:41 -0400

    updated eventDisplay

--- client/src/screens/EventDisplay.jsx
+++ client/src/screens/EventDisplay.jsx
@@ -46,27 +46,27 @@ export default function EventDisplay(props) {
     >
 
 
-      {events.map((event) => (
 <Box
 sx={{
   margin: 1,
-width: "200px",
+  width: "200px",
   alignItems: "center",
   justifyContent: "center",
   border: 3,
   borderRadius: 6,
   fontSize: "20px"
 }}>
+{events.map((event) => (
         <div className="event-display" key={event.id}>
           <p> {event.name}</p>
           <p>{event.place}</p>
           <p> {event.date}</p>
           <p>{event.time}</p>
         </div>
+          ))}
       <Link to>RSVP</Link>
 
 </Box>
-      ))}
     
           </Box>
           </Container>
