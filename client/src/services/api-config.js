36dbc698bc5961ebac2bb50de56513eb4a45d504
guess01325 <guess01325@gmail.com>
2023-11-01 10:35:53 -0400

    updated services url

--- client/src/services/api-config.js
+++ client/src/services/api-config.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const baseUrl = 
 process.env.NODE_ENV === "production" 
- ? "https://inspiring-4b6db7e6a01c.herokuapp.com/" : "http://localhost:3000" 
+ ? "https://inspiring-4b6db7e6a01c.herokuapp.com" : "http://localhost:3000" 
 
  const api = axios.create({
   baseURL: baseUrl
