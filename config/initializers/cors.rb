ed286f6730963f7d2714f72b55b187f652925efb
guess01325 <guess01325@gmail.com>
2024-10-23 16:12:29 -0400

    changed cors url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "clinquant-cranachan-d8dc7b.netlify.app"
+    origins "https://inspiring-2.netlify.app"
 
     resource "*",
       headers: :any,
